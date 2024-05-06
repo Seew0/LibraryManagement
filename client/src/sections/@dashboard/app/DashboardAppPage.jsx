9320730ef81e8c2a880e7c88f90e428de6fe835d
seew0 <miglanidevansh83@gmail.com>
2024-05-06 13:46:51 +0530

    is it?

--- client/src/sections/@dashboard/app/DashboardAppPage.jsx
+++ client/src/sections/@dashboard/app/DashboardAppPage.jsx
@@ -78,23 +78,23 @@ export default function DashboardAppPage() {
             />
           </Grid>
 
-          <Grid item xs={12} md={6} lg={4}>
-            <AppCurrentVisits
-              title="Current Visits"
-              chartData={[
-                { label: 'America', value: 4344 },
-                { label: 'Asia', value: 5435 },
-                { label: 'Europe', value: 1443 },
-                { label: 'Africa', value: 4443 },
-              ]}
-              chartColors={[
-                theme.palette.primary.main,
-                theme.palette.info.main,
-                theme.palette.warning.main,
-                theme.palette.error.main,
-              ]}
-            />
-          </Grid>
+            <Grid item xs={12} md={6} lg={4}>
+              <AppCurrentVisits
+                title="Current Visits"
+                chartData={[
+                  { label: 'America', value: 4344 },
+                  { label: 'Asia', value: 5435 },
+                  { label: 'Europe', value: 1443 },
+                  { label: 'Africa', value: 4443 },
+                ]}
+                chartColors={[
+                  theme.palette.primary.main,
+                  theme.palette.info.main,
+                  theme.palette.warning.main,
+                  theme.palette.error.main,
+                ]}
+              />
+            </Grid>
 
         </Grid>
       </Container>
